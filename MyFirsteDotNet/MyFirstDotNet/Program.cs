bdfc1837e5a602f0c8f3b33c1955af57558de613
Dbeidels <dbeidels@macalester.edu>
2022-07-06 14:24:50 -0500

    add Myfirstdotnet

---
+++ MyFirsteDotNet/MyFirstDotNet/Program.cs
@@ -0,0 +1,12 @@
+
+//This code make it say hello world
+
+﻿// See https://aka.ms/new-console-template for more information
+
+//console
+//"" string
+//; endline charcter
+
+
+
+Console.WriteLine("Hello, World!");
