15401c6c43b7f7f22d4fbc4bc95a06b3124f48c2
Dbeidels <dbeidels@macalester.edu>
2022-07-07 19:05:33 -0500

    added player action

--- P0/Program.cs
+++ P0/Program.cs
@@ -7,15 +7,32 @@ namespace Game{
         
 
         static void Main(string[] args){
-            Console.WriteLine("Welcome to Dragon Battle \nChoose your Class \n1.Warrior\n2.Mage\n3.Gunslinger");
-            int response = int.Parse(Console.ReadLine());
             Dragon dragon  = new Dragon();
             Player player  = new Player();
-            player.setClass(response);
+            bool test;
+            int classname;
+            do{
+                
+                Console.WriteLine("Welcome to Dragon Battle \nChoose your Class by selecting the number next to the class \n1.Warrior\n2.Mage\n3.Gunslinger");
+                string input = Console.ReadLine();
+                test = int.TryParse(input, out classname) && (input == "1" || input == "2" || input == "3"); 
+            }while(!test);
+            
+            player.setClass(classname);          
+            
             while(dragon.health >0 && player.health > 0){
-                Console.WriteLine("HP:" + player.health + "   SP:" + player.sp +  "\nAttack   " + player.skill + "    Defend");
-                Console.ReadLine();
-                dragon.health -=30;
+                Console.WriteLine("HP:" + player.health + "   SP:" + player.sp +  "\nAttack   " + player.skill +  "      Heal");
+                string action = Console.ReadLine();
+                string skill = player.skill;
+                if(action == "Attack"){
+                    dragon.health -= player.attack();
+                 }
+                else if(action == skill){
+                    dragon.health -= player.skillattack();
+                }
+                else if(action == "Heal"){
+                    player.heal();
+                }
                 Console.WriteLine("player health" + player.health + " dragon health" + dragon.health);
                 
             }
