e5c59a7a5882955f2444f5403d8527efd4d2b132
Dbeidels <dbeidels@macalester.edu>
2022-07-07 20:26:49 -0500

    finished player class

--- P0/Player.cs
+++ P0/Player.cs
@@ -25,14 +25,14 @@ class Player{
                 skill = "Slash";
                 dmglow = 10;
                 dmghigh = 12;
-                skilldmglow = 10;
-                skilldmghigh = 12;
+                skilldmglow = 15;
+                skilldmghigh = 20;
                 break;
             case 2:
                 health = 36;
                 maxhealth = 36;
                 sp = 35;
-                skill = "Magic Missle";
+                skill = "Magic Missile";
                 dmglow = 4;
                 dmghigh = 6;
                 skilldmglow = 21;
@@ -55,13 +55,33 @@ class Player{
 
     }
     public int attack(){
-        return rand.Next(dmglow,dmghigh +1);
+        int dmg = rand.Next(dmglow,dmghigh +1);
+        if(maxhealth == 50){
+            Console.WriteLine("\nYou swipe at the dragon with your axe for " + dmg + " damage");
+        }
+        else if(maxhealth == 36){
+            Console.WriteLine("\nYou swipe your dagger at the dragon for " + dmg + " damage");
+        }
+        else{
+            Console.WriteLine("\nYou shoot your pistols at the dragon for " + dmg + " damage");
+        }
+        return dmg;
     }
 
     public int skillattack(){
         if(sp!= 0){
             sp -= 5;
-            return rand.Next(skilldmglow,skilldmghigh +1);
+            int dmg = rand.Next(skilldmglow,skilldmghigh +1);
+            if(maxhealth == 50){
+                Console.WriteLine("\nYou slash at the dragon's neck with your axe for " + dmg + " damage");
+            }
+            else if(maxhealth == 36){
+                Console.WriteLine("\nYou cast your arcane energy at the dragon for " + dmg + " damage");
+            }
+            else{
+                Console.WriteLine("\nYou aim your musket between the dragon's eyes for " + dmg + " damage");
+            }
+            return dmg;
         }
         return 0;
     }
@@ -75,6 +95,9 @@ class Player{
            else{
             health +=15;
            }
+        }
+        else{
+            Console.WriteLine("\nYou wasted your turn trying to drink an empty bottle");
         }
     }    
 
