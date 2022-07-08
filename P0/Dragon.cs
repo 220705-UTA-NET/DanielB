a1ff5b4aed0ef97cbccc057564ccbc61e271eaf2
Dbeidels <dbeidels@macalester.edu>
2022-07-07 20:27:40 -0500

    dragon class finished

--- P0/Dragon.cs
+++ P0/Dragon.cs
@@ -3,7 +3,26 @@ using System;
 
 
     class Dragon{
+        Random rand = new Random();
         public int health = 100;
+        
+        public int attack(){
+            int randomattack = rand.Next(0,100);
+            if(randomattack<25){
+                Console.WriteLine("The dragon swipes and just misses you");
+                return 0;
+            }
+            else if(randomattack>=25 && randomattack <75){
+                int dmg = rand.Next(8,11);
+                Console.WriteLine("The dragon swipes at you and hits you for " + dmg + " damage");
+                return dmg;
+            }
+            else{
+                int dmg = rand.Next(16, 21);
+                Console.WriteLine("The dragon breathes fire on you for " + dmg + " damage");
+                return dmg;
+            }
+        }
 
         
 
