88ba1afa2231717a5275d411904f451210e8c942
Dbeidels <dbeidels@macalester.edu>
2022-07-11 16:25:37 -0500

    introducing display menu

--- P0/Program.cs
+++ P0/Program.cs
@@ -7,62 +7,72 @@ namespace ProjectZero{
         
 
         static void Main(string[] args){
-            Mage player = new Mage();
-            player.createSkillList();
-            
-            player.displaySkillList();
-            Console.WriteLine("Done");
-            // Dragon dragon  = new Dragon();
-            // bool test;
-            // int classname;
-            // do{
+            bool test;
+            int classname;
+            Dictionary<string, int> h = new Dictionary<string, int>();
+            do{
                 
-            //     Console.WriteLine("\nWelcome to Dragon Battle \nChoose your Class by typing the number next to the class \n1.Warrior      HP:50 SP:15\n2.Mage         HP:36 SP:25\n3.Gunslinger   HP:43 SP:20\n");
-            //     string input = Console.ReadLine();
-            //     test = int.TryParse(input, out classname) && (input == "1" || input == "2" || input == "3"); 
-            // }while(!test);
-            // switch(classname){
-            //     case 1:
-            //         Mage player = new Mage();
-            //         break;
-            // }
-            // // Player player  = new Player();      
-            // string action;
-            // // string skills = player.skill;
-            // // skills = skills.ToLower();
-            // while(dragon.health >0 && player.health > 0){
-            //     do{
-            //         Console.WriteLine("\nDragon Health:"+dragon.displayHealth()+"\n \nHP:" + player.health + "/"+player.maxhealth+"   SP:" + player.sp + "   Health Potions:" + player.healthpot +  "\n \nActions: \nAttack   " + player.skill +  "(Uses 5 SP)      Heal(Heals 15)");
-            //         action = Console.ReadLine();
-            //         action = action.ToLower();
-            //         action = action.TrimEnd();
-            //         test = (action=="attack" || action == skills || action == "heal");
-            //     }while(!test);
+                Console.WriteLine("\nWelcome to Dragon Battle \nChoose your Class by typing the number next to the class \n1.Warrior      HP:50 SP:15\n2.Mage         HP:36 SP:25\n3.Gunslinger   HP:43 SP:20\n");
+                string input = Console.ReadLine();
+                test = int.TryParse(input, out classname) && (input == "1" || input == "2" || input == "3"); 
+            }while(!test);
+            Player player = createClass(classname);
+            // h = player.createSkillList();
+            Dragon dragon  = new Dragon();
+            Console.WriteLine(player.health);
 
+            // Player player  = new Player();      
+            string action;
+            while(dragon.health >0 && player.health > 0){
+                do{
+                    Console.WriteLine("Test");
+                    Console.WriteLine("\nDragon Health:"+dragon.displayHealth()+"\n \nHP:" + player.health + "/"+player.maxhealth+"   SP:" + player.sp + "   Health Potions:" + player.healthpot +  "\n \nActions: \nAttack   Skills      Heal(Heals 15)");
+                    action = Console.ReadLine();
+                    action = action.ToLower();
+                    action = action.TrimEnd();
+                    test = (action=="attack" || action == "skills" || action == "heal");
+                }while(!test);
                 
-            //     if(action == "attack"){
-            //         dragon.health -= player.attack();
-            //      }
-            //     else if(action == skills){
-            //         dragon.health -= player.skillattack();
-            //     }
-            //     else if(action == "heal"){
-            //         player.heal();
-            //     }
-            //     player.health -= dragon.attack();
-            // }
-            // // bool trueending = (dragon.health <=0 && player.health>0);
-            // // bool badending = (dragon.health >0 && player.health<=0);
-            // // bool herosending =(dragon.health <=0 && player.health<=0);
-            // if(dragon.health<=0){
-            //     Console.WriteLine("\nThe dragon is defeated and the town is safe. \nYOU WIN!");
-            // }
-            // else if(player.health<=0){
-            //     Console.WriteLine("\nThe dragon eats you and the town is burned \nYOU DIED");
-            // }
+                
+                if(action == "attack"){
+                    dragon.health -= player.attack();
+                }
+                else if(action == "skills"){
+                    dragon.health -= player.skillattack(h);
+                }
+                else if(action == "heal"){
+                    player.heal();
+                }
+                player.health -= dragon.attack();
+            }
+            // bool trueending = (dragon.health <=0 && player.health>0);
+            // bool badending = (dragon.health >0 && player.health<=0);
+            // bool herosending =(dragon.health <=0 && player.health<=0);
+            if(dragon.health<=0){
+                Console.WriteLine("\nThe dragon is defeated and the town is safe. \nYOU WIN!");
+            }
+            else if(player.health<=0){
+                Console.WriteLine("\nThe dragon eats you and the town is burned \nYOU DIED");
+            }
             
         }
 
+        static Player createClass(int classname){
+            if(classname == 1){
+                    Warrior player = new Warrior();
+                    return player;
+            }
+            else if(classname == 2){
+                    Mage player = new Mage();
+                    return player;
+
+            }
+            else if(classname == 2){
+                    Gunslinger player = new Gunslinger();
+                    return player;
+            }
+            return null;
+        } 
         
     }
 }
