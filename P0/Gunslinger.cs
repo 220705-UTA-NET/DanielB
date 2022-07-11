721bcb533d460c52ab8a794051d9d729063c0126
Dbeidels <dbeidels@macalester.edu>
2022-07-11 16:24:33 -0500

    add new and modified methods

--- P0/Gunslinger.cs
+++ P0/Gunslinger.cs
@@ -13,10 +13,11 @@ namespace ProjectZero{
         // private int skilldmglow;
         // private int skilldmghigh;
 
-        IDictionary<string, ((int,int),int)> skillList = new Dictionary<string, ((int,int),int)>();       
-        public void createSkillList(){
-            skillList.Add("Snipe Shot", ((11,25), 5));
-            skillList.Add("Trick Shot",((45,50),15));
+        Dictionary<string, int> skillList = new Dictionary<string, int>();       
+        public Dictionary<string, int> createSkillList(){
+            skillList.Add("1:Snipe Shot", 5); //11,25),
+            skillList.Add("2:Trick Shot",15); //(45,50),
+            return skillList;
         }
         public int attack(){
             //  return the dmg number for the basic attack based on the class
@@ -24,9 +25,11 @@ namespace ProjectZero{
             Console.WriteLine("\nYou shoot your pistols at the dragon for " + dmg + " damage.");
             return dmg;    
         }
-        // public int skillattack(){
-        //     // checks to see if you have enough sp to return the dmg number for the skill based on the class
-        //     displaySkillList();
+        public void skillattack(Dictionary<string, int> h){
+            // checks to see if you have enough sp to return the dmg number for the skill based on the class
+            Console.WriteLine("Type the number next the action you want to do");
+            displaySkillList(h);
+            string action = Console.ReadLine();
 
             
         //         sp -= 5;
@@ -39,6 +42,6 @@ namespace ProjectZero{
             
         //     Console.WriteLine("\nYou try to use your skill but you are too tired to manage it.");
             
-        // }
+        }
     }
 }
