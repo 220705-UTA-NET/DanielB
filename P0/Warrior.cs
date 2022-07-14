58ef598e4e05ca40e1857f27a8d00d4b815721c1
Dbeidels <dbeidels@macalester.edu>
2022-07-13 19:59:56 -0500

    finished updating the warrio's skill list and attributes

--- P0/Warrior.cs
+++ P0/Warrior.cs
@@ -5,43 +5,74 @@ namespace ProjectZero{
 
     class Warrior : Player{
         Random rand = new Random();
-        public int health = 50;
-        public int maxhealth =50;
-        public int sp = 15;
+        public int health;
+        public int maxhealth;
+        public int sp;
         private int dmglow = 10;
-        private int dmghigh = 12;
-        // private int skilldmglow;
-        // private int skilldmghigh;
+        private int dmghigh = 15;
+        
 
+        override public void initializeClass(){
+            this.setHealth(50);
+            this.setSp(15);
+            this.setMaxHealth(50);
+        }
         Dictionary<string, int> skillList = new Dictionary<string, int>();       
-        public Dictionary<string, int> createSkillList(){
+        override public Dictionary<string, int> createSkillList(){
             skillList.Add("1:Slash", (5)); //(15,20),
-            skillList.Add("2:Lightning Cleave",10); //(30,40),
+            skillList.Add("2:Lightning Cleave",15); //(30,40),
             return skillList;
         }
-        public int attack(){
+        override public int attack(){
             //  return the dmg number for the basic attack based on the class
             int dmg = rand.Next(dmglow,dmghigh +1);              
             Console.WriteLine("\nYou swipe at the dragon with your axe for " + dmg + " damage");
             return dmg;    
         }
-        public void skillattack(Dictionary<string, int> h){
+        override public int skillattack(Dictionary<string, int> h){
             // checks to see if you have enough sp to return the dmg number for the skill based on the class
-            Console.WriteLine("Type the number next the action you want to do");
-            displaySkillList(h);
-            string action = Console.ReadLine();
-
             
-        //         sp -= 5;
-        //         int dmg = rand.Next(skilldmglow,skilldmghigh +1);
-        //         // switch(classname){
-        //         // case 2:                
-        //         Console.WriteLine("\nYou cast your arcane energy at the dragon for " + dmg + " damage.");
-        //         return dmg;
-                
-            
-        //     Console.WriteLine("\nYou try to use your skill but you are too tired to manage it.");
-            
+            bool test;
+            string action;
+            int name;
+            string keyskill;
+            do{
+                Console.WriteLine("\nSP:"+this.getSP() + "\nType the number next the action you want to do");
+                displaySkillList(h);
+                action = Console.ReadLine();
+                action = action.ToLower();
+                action = action.TrimEnd();
+                test = int.TryParse(action, out name) && (action == "1" || action == "2");
+                if (name == 1){
+                    keyskill = "1:Slash";
+                }
+                else if (name == 2){
+                    keyskill = "2:Lightning Cleave";
+                }
+                else{
+                    keyskill = "";
+                }
+            }while(!test);
+            if(this.getSP() >= h[keyskill]){
+                switch(name){
+                    case 1:
+                        int dmg = rand.Next(15,20);
+                        this.setSp(this.getSP() - h[keyskill]);
+                        Console.WriteLine("\nYou slash at the dragon's neck with your axe for " + dmg + " damage.");
+                        return dmg;
+                    case 2:
+                        dmg = rand.Next(30,60);
+                        this.setSp(this.getSP() - h[keyskill]);
+                        Console.WriteLine("\nYou summon lightning on to you axe to cleave at the dragon's head for " + dmg + " damage.");
+                        return dmg;
+                    default:
+                        return 0;
+                }
+            }
+            else{
+                Console.WriteLine("You do not have enough SP to use that skill");
+                return 0;
+            }
         }
     }
 }
