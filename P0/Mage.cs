641d5a8f4c6083e43169c62ebe485a4d414b4845
Dbeidels <dbeidels@macalester.edu>
2022-07-13 20:00:36 -0500

    finished updating the mage's skill list and attributes

--- P0/Mage.cs
+++ P0/Mage.cs
@@ -5,33 +5,80 @@ namespace ProjectZero{
 
     class Mage : Player{
         Random rand = new Random();
-        public int health = 36;
-        public int maxhealth =36;
-        public int sp = 25;
+        public int health;
+        public int maxhealth;
+        public int sp;
         private int dmglow = 4;
         private int dmghigh = 6;
         // private int skilldmglow;
         // private int skilldmghigh;
 
+        override public void initializeClass(){
+            this.setHealth(36);
+            this.setSp(25);
+            this.setMaxHealth(36);
+        }
+
         Dictionary<string, int> skillList = new Dictionary<string, int>();       
-        public Dictionary<string, int> createSkillList(){
-            skillList.Add("1:Magic Missle", (5)); //(21,25),
+        override public Dictionary<string, int> createSkillList(){
+            skillList.Add("1:Magic Missile", (5)); //(21,25),
             skillList.Add("2:Fireball",(15)); //(45,50),
             return skillList;
 
         }
-        public int attack(){
+        override public int attack(){
             //  return the dmg number for the basic attack based on the class
             int dmg = rand.Next(dmglow,dmghigh +1);              
             Console.WriteLine("\nYou swipe your dagger at the dragon for " + dmg + " damage.");
             return dmg;    
         }
-        public void skillattack(Dictionary<string, int> h){
+        override public int skillattack(Dictionary<string, int> h){
             // checks to see if you have enough sp to return the dmg number for the skill based on the class
-            Console.WriteLine("Type the number next the action you want to do");
-            displaySkillList(h);
-            string action = Console.ReadLine();
-            
+            bool test;
+            string action;
+            int name;
+            string keyskill;
+            do{
+                Console.WriteLine("\nSP:"+this.getSP()+ "\nType the number next the action you want to do");
+                displaySkillList(h);
+                action = Console.ReadLine();
+                action = action.ToLower();
+                action = action.TrimEnd();
+                test = int.TryParse(action, out name) && (action == "1" || action == "2");
+                if (name == 1){
+                    keyskill = "1:Magic Missile";
+                }
+                else if (name == 2){
+                    keyskill = "2:Fireball";
+                }
+                else{
+                    keyskill = "";
+                }
+            }while(!test);
+            if(keyskill == ""){
+                Console.WriteLine("HI");
+                return 0;
+            }
+            else if(this.getSP() >= h[keyskill]){
+                switch(name){
+                    case 1:
+                        int dmg = rand.Next(21,25);
+                        this.setSp(this.getSP() - h[keyskill]);
+                        Console.WriteLine("\nYou cast your arcane energy bolts at the dragon for " + dmg + " damage.");
+                        return dmg;
+                    case 2:
+                        dmg = rand.Next(45,50);
+                        this.setSp(this.getSP() - h[keyskill]);
+                        Console.WriteLine("\nYou summon a fiery ball of energy to char the dragon " + dmg + " damage.");
+                        return dmg;
+                    default:
+                        return 0;
+                }
+            }
+            else{
+                Console.WriteLine("You do not have enough SP to use that skill");
+                return 0;
+            }
             
         //         sp -= 5;
         //         int dmg = rand.Next(skilldmglow,skilldmghigh +1);
